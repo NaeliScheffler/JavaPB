17500fa2be48c95207dca8d2da763ee44ef587ec
Joaoenaeli05 <naelisdasilva@gmail.com>
2023-12-27 20:16:31 -0300

    commitV3

---
+++ atividades/src/excecoes_v3/exceptions.java
@@ -0,0 +1,9 @@
+package excecoes_v3;
+
+public class exceptions extends RuntimeException {
+	private static final long serialVersionUID = 1L;
+
+	public exceptions(String msg) {
+		super(msg);
+	}
+}
