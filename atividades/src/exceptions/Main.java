5835cd8cb4646bcf3f0bb280111e55cb2dfb3d91
Joaoenaeli05 <naelisdasilva@gmail.com>
2023-12-27 20:49:55 -0300

    commit

--- atividades/src/exceptions/Main.java
+++ atividades/src/exceptions/Main.java
@@ -1,10 +1,39 @@
 package exceptions;
 
+import java.util.Scanner;
+import exceptions.BusinessExceptions;
+
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+		Scanner sc = new Scanner(System.in);
+
+		System.out.println("Informe os dados da conta");
+		System.out.print("Numero: ");
+		int number = sc.nextInt();
+		System.out.print("Titular: ");
+		sc.nextLine();
+		String holder = sc.nextLine();
+		System.out.print("Saldo inicial: ");
+		double balance = sc.nextDouble();
+		System.out.print("Limite de saque: ");
+		double withdrawLimit = sc.nextDouble();
+
+		Account acc = new Account(number, holder, balance, withdrawLimit);
+
+		System.out.println();
+		System.out.print("Informe uma quantia para sacar: ");
+		double amount = sc.nextDouble();
+
+		try {
+			acc.withdraw(amount);
+			System.out.printf("Novo saldo: %.2f%n", acc.getBalance());
+		} catch (BusinessExceptions e) {
+			System.out.println(e.getMessage());
+		}
+
+		sc.close();
 	}
 
 }
